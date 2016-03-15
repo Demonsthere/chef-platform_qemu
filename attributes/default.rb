e810bf16c7a029447f9dd47d2c71aa2b46b2f578
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-03-15 11:25:21 +0100

    added repo linaro

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,3 @@
 default['platform_qemu']['release'] = 'jessie'
+default['platform_qemu']['linaro_key'] = '7BE1F97B'
+default['platform_qemu']['packages'] = %w(linaro-media-create qemu debootstrap)
--- recipes/preparation.rb
+++ recipes/preparation.rb
@@ -6,13 +6,20 @@ template '/etc/apt/sources.list.d/linaro.list' do
   action :create_if_missing
 end
 
+execute 'Get repository key' do
+  command "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys #{node['platform_qemu']['linaro_key']}"
+  creates '/tmp/something'
+  action :run
+end
+
+
 execute 'update repository cache' do
   command 'apt-get update'
   action :run
 end
 
-['software-properties-common', 'python-software-properties'].each do |pkg|
-  package pkg do
-    action :upgrade
-  end
-end
+# ['software-properties-common', 'python-software-properties'].each do |pkg|
+#   package pkg do
+#     action :upgrade
+#   end
+# end
