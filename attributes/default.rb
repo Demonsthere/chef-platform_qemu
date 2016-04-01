7286a68dec647497d058435f3cdf32ee6e02c982
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-04-01 12:58:42 +0200

    added missing package

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['platform_qemu']['release'] = 'stable'
-default['platform_qemu']['packages'] = %w(python qemu linaro-image-tools debootstrap xvfb gdisk util-linux)
+default['platform_qemu']['packages'] = %w(python qemu linaro-image-tools debootstrap xvfb gdisk util-linux binfmt-support)
 default['platform_qemu']['packer_version'] = '0.10.0'
 default['platform_qemu']['packer_url'] = "https://releases.hashicorp.com/packer/#{node['platform_qemu']['packer_version']}/packer_#{node['platform_qemu']['packer_version']}_linux_amd64.zip"
