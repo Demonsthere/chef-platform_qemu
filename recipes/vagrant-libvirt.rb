70795c6fa4e7543f3fe9a035af2d902eec6327fe
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-06-18 15:58:55 +0200

    fixed old params

--- recipes/vagrant-libvirt.rb
+++ recipes/vagrant-libvirt.rb
@@ -1,19 +1,19 @@
-remote_file "#{Chef::Config[:file_cache_path]}/vagrant_#{node['platform_qemu']['vagrant_version']}_x86_64.deb" do
+remote_file "#{Chef::Config[:file_cache_path]}/vagrant_#{node[:platform_qemu][:vagrant_version]}_x86_64.deb" do
   owner 'root'
   group 'root'
   mode '0644'
-  source node['platform_qemu']['vagrant_url']
+  source node[:platform_qemu][:vagrant_url]
   action :create_if_missing
 end
 
-dpkg_package "vagrant_#{node['platform_qemu']['vagrant_version']}_x86_64.deb" do
-  source "#{Chef::Config[:file_cache_path]}/vagrant_#{node['platform_qemu']['vagrant_version']}_x86_64.deb"
+dpkg_package "vagrant_#{node[:platform_qemu][:vagrant_version]}_x86_64.deb" do
+  source "#{Chef::Config[:file_cache_path]}/vagrant_#{node[:platform_qemu][:vagrant_version]}_x86_64.deb"
 end
 
-node['platform_qemu']['vagrant_plugins'].each do |plugin|
+node[:platform_qemu][:vagrant_plugins].each do |plugin|
   execute 'Install librivt plugin for vagrant' do
     command "vagrant plugin install #{plugin}"
-    user 'vagrant'
+    user node[:platform_qemu][:user]
     action :run
   end
 end
