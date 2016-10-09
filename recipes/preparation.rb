09405dfdcbe60ddf6b1bea3902ec3b3d85560318
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-10-09 19:54:42 +0200

    removed legacy code

--- recipes/preparation.rb
+++ recipes/preparation.rb
@@ -13,13 +13,11 @@ end
 node[:platform_qemu][:packages].each do |pkg|
   apt_package pkg do
     options '-o APT::Force-LoopBreak=1'
-    default_release 'jessie'
   end
 end
 
 node[:platform_qemu][:linaro_dep].each do |dep|
   apt_package dep do
-    default_release 'jessie'
   end
 end
 
