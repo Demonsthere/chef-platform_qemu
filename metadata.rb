27d0743445d6097b70d00166d232a9eb85afb4c9
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-11-06 12:29:03 +0100

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jakub.blaszczyk@sap.com'
 license 'all_rights'
 description 'Installs/Configures chef-platform_qemu'
 long_description 'Installs/Configures chef-platform_qemu'
-version '0.1.7'
+version '0.1.8'
 
 depends 'platform_base'
